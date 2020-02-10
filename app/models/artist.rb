950fe86e5efe7788d3fced3aac1a0f821c505e71
Matthew Jagiello <mjagiello@gmail.com>
2020-02-10 04:53:15 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,7 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-
+    self.songs.first.genre
   end
 
   def song_count
@@ -11,6 +11,6 @@ class Artist < ActiveRecord::Base
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+    self.genres.length
   end
 end
